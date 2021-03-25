67d2ce55ddee46704beca7ca06124440c5bfaa96
outlaw <outlawstar4761@gmail.com>
2021-03-25 17:47:32 -0500

    multithreading test

--- test.py
+++ test.py
@@ -4,49 +4,44 @@ from ChanParser.ChanParser import ChanParser
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from multiprocessing import Pool
 import os
+import random
 
 def _readFile(path):
     f = open(path)
     html = f.read()
     f.close()
     return html
-
-chatbot = ChatBot("ChanChatter")
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train('chatterbot.corpus.english')
-
-threads = [
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293176174/293176174.html',
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293192259/293192259.html',
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293199125/293199125.html',
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293206224/293206224.html',
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293213332/293213332.html',
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293213534/293213534.html',
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293214560/293214560.html',
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293215202/293215202.html',
-    '/media/outlaw/BlackBox1/archive.4plebs.org/pol/293215692/293215692.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79653672/79653672.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79676249/79676249.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79679605/79679605.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79682719/79682719.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79686390/79686390.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79687668/79687668.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79688031/79688031.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79693414/79693414.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79694546/79694546.html',
-    '/media/outlaw/BlackBox1/boards.4channel.org/g/79695091/79695091.html'
-]
-
 def _train(thread):
     html = _readFile(thread)
     parser = ChanParser(html)
     trainer = ListTrainer(chatbot)
     trainer.train(parser.posts)
+def _findFiles(input):
+    rootDir = input[0]
+    extension = input[1]
+    results = []
+    for root,dirs,files in os.walk(rootDir):
+        for file in files:
+            if file.endswith(extension):
+                #print(os.path.join(root,file))
+                results.append(os.path.join(root,file))
+    return results
 
+chatbot = ChatBot("ChanChatter")
+trainer = ChatterBotCorpusTrainer(chatbot)
+#trainer.train('chatterbot.corpus.english')
 
+limit = 500
+path = '/media/outlaw/BlackBox1/archive.4plebs.org/pol/'
+targetData = [path,".html"]
 p = Pool(os.cpu_count())
+threads = p.apply(_findFiles,(targetData,))
+print("Found: %s" % len(threads))
+threads = random.sample(threads, limit)
 p.map(_train, threads)
 
 while True:
    data = input("Say: ")
+   if(data == "stop"):
+       exit()
    print(chatbot.get_response(data))
